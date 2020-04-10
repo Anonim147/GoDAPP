b7d51f9f7f9178a3de69dfcaae0405a9be34476d
Alex Mykhailiuk <AleXXX14199@gmail.com>
2020-04-10 19:55:07 +0300

    created first router of api

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"GODAPP/router"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("First commit")
+	//fmt.Println("First commit")
+	r := router.Router()
+	fmt.Println("Start listening on 9000...")
+	log.Fatal(http.ListenAndServe(":9000", r))
 }
--- router/router.go
+++ router/router.go
@@ -1,7 +1,7 @@
 package router
 
 import (
-	//"goapp2/middleware"
+	"GODAPP/middleware"
 
 	"github.com/gorilla/mux"
 )
@@ -9,8 +9,8 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	/*router.HandleFunc("/api/user/{id}", middleware.GetUser).Methods("GET", "OPTIONS")
-	router.HandleFunc("/api/user/", middleware.GetAllUser).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/get_columns/{table}", middleware.GetTableKeys).Methods("GET", "OPTIONS")
+	/*router.HandleFunc("/api/user/", middleware.GetAllUser).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/newuser", middleware.CreateUser).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/user/{id}", middleware.UpdateUser).Methods("PUT", "OPTIONS")
 	router.HandleFunc("/api/deleteuser/{id}", middleware.DeleteUser).Methods("DELETE", "OPTIONS")
