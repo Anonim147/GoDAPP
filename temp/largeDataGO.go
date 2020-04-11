a56d7545f092d66193326ebb89a1b1be59412efe
Alex Black <AleXXX14199@gmail.com>
2020-04-11 15:42:32 +0300

    added goroutine func for go

---
+++ temp/largeDataGO.go
@@ -0,0 +1,21 @@
+func method3(db *gorm.DB) {
+    records := make([]Record, 0)
+    defer timeTrack(time.Now(), "Method 3")
+    count := 1000000
+    bucketSize := 100000
+    resultCount := 0
+    resultChannel := make(chan []Record, 0)
+    for beginID :=1; beginID <=count; beginID += bucketSize {
+        endId := beginID +bucketSize
+        go func(beginId int, endId int) {
+            currentRecords := make([]Record, 0)
+            db.Table("records").Select("id").Where("id>=? and id<?", beginId, endId).Find(&currentRecords)
+            resultChannel <- currentRecords
+        }(beginID, endId)
+        resultCount += 1
+    }
+    for i:=0; i<resultCount; i++{
+        currentRecords := <- resultChannel
+        records = append(records, currentRecords...)
+    }
+}
